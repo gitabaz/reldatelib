653f2365343db93def2326c479a887e0b14d4335
Aidan Zabalo <olabaz@gmail.com>
2021-02-12 12:01:25 -0500

    gofmt and refactor TestFail

--- reldatelib.go
+++ reldatelib.go
@@ -10,10 +10,10 @@ import "strconv"
 // TODO: Rewrite this to avoid having to duplicate the keys
 var relTimeOrdKeys = []string{"LAST", "THIS", "NEXT", "FIRST", "THIRD", "FOURTH", "FIFTH", "SIXTH", "SEVENTH", "EIGHTH", "NINTH", "TENTH", "ELEVENTH", "TWELFTH"}
 var relTimeOrdinal = map[string]int{
-	"LAST":  -1,
-	"THIS":  0,
-	"NEXT":  1,
-	"FIRST": 1,
+	"LAST":     -1,
+	"THIS":     0,
+	"NEXT":     1,
+	"FIRST":    1,
 	"THIRD":    3,
 	"FOURTH":   4,
 	"FIFTH":    5,
@@ -31,76 +31,76 @@ var relTimeUnit = []string{"SEC", "SECOND", "MIN", "MINUTE", "HOUR", "DAY", "WEE
 // TODO: Rewrite this to avoid having to duplicate the keys
 var relTimeAgoKeys = []string{"AGO", "HENCE"}
 var relTimeAgo = map[string]int{
-    "AGO": -1,
-    "HENCE": 1,
+	"AGO":   -1,
+	"HENCE": 1,
 }
 
 var relTimeShift = []string{"NOW", "TODAY", "YESTERDAY", "TOMORROW"}
 
 var weekdayKeys = []string{"MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY", "SUNDAY"}
 var weekdays = map[string]int{
-    "MONDAY": 0,
-    "TUESDAY": 1,
-    "WEDNESDAY": 2,
-    "THURSDAY": 3,
-    "FRIDAY": 4,
-    "SATURDAY": 5,
-    "SUNDAY": 6}
+	"MONDAY":    0,
+	"TUESDAY":   1,
+	"WEDNESDAY": 2,
+	"THURSDAY":  3,
+	"FRIDAY":    4,
+	"SATURDAY":  5,
+	"SUNDAY":    6}
 
 var regexStr = "(?P<num>-?\\d+|" + strings.Join(relTimeOrdKeys, "|") +
-               ")?\\s?(?P<unit>" + strings.Join(relTimeUnit, "|") + "|" + strings.Join(weekdayKeys, "|") +
-               ")S?\\b\\s?(?P<ago>" + strings.Join(relTimeAgoKeys, "|") + ")?|^(" + strings.Join(relTimeShift, "|") + ")$"
+	")?\\s?(?P<unit>" + strings.Join(relTimeUnit, "|") + "|" + strings.Join(weekdayKeys, "|") +
+	")S?\\b\\s?(?P<ago>" + strings.Join(relTimeAgoKeys, "|") + ")?|^(" + strings.Join(relTimeShift, "|") + ")$"
 var r, _ = regexp.Compile(regexStr)
 
 func Parse(str string) (string, error) {
 	str = strings.ToUpper(str)
 	format := "Mon 02 Jan 2006 03:04:05 PM MST"
 
-    res := r.FindAllStringSubmatch(str, -1)
+	res := r.FindAllStringSubmatch(str, -1)
 
-    var relativeTime = make(map[string]int)
-    t := time.Now().UTC()
-    if len(res) == 0 {
-        return "", errors.New("Could not parse string")
-    }
-    for _, m := range res {
-        if m[0] == m[4] {
-            relativeTime[m[4]] = 1
-        } else {
-            numUnits := 1
-            var err error
-            if m[1] != "" {
-                numUnits, err = strconv.Atoi(m[1])
-                if err != nil {
-                    var ok = false
-                    numUnits, ok = relTimeOrdinal[m[1]]
-                    if ok == false {
-                        return "", errors.New("Could not parse string")
-                    }
-                }
-            }
-            prefactor, ok := relTimeAgo[m[3]]
-            if (ok == false) {
-                prefactor = 1
-            }
-            wd, ok := weekdays[m[2]]
-            if ok == true {
-                relWeekday := weekdays[strings.ToUpper(t.Weekday().String())] - wd
-                offset := 0
-                if (numUnits * relWeekday >= 0) {
-                    offset = prefactor * numUnits * 7 - relWeekday
-                } else {
-                    offset = prefactor * (int(math.Abs(float64(numUnits))) - 1) * 7 - relWeekday
-                }
+	var relativeTime = make(map[string]int)
+	t := time.Now().UTC()
+	if len(res) == 0 {
+		return "", errors.New("Could not parse string")
+	}
+	for _, m := range res {
+		if m[0] == m[4] {
+			relativeTime[m[4]] = 1
+		} else {
+			numUnits := 1
+			var err error
+			if m[1] != "" {
+				numUnits, err = strconv.Atoi(m[1])
+				if err != nil {
+					var ok = false
+					numUnits, ok = relTimeOrdinal[m[1]]
+					if ok == false {
+						return "", errors.New("Could not parse string")
+					}
+				}
+			}
+			prefactor, ok := relTimeAgo[m[3]]
+			if ok == false {
+				prefactor = 1
+			}
+			wd, ok := weekdays[m[2]]
+			if ok == true {
+				relWeekday := weekdays[strings.ToUpper(t.Weekday().String())] - wd
+				offset := 0
+				if numUnits*relWeekday >= 0 {
+					offset = prefactor*numUnits*7 - relWeekday
+				} else {
+					offset = prefactor*(int(math.Abs(float64(numUnits)))-1)*7 - relWeekday
+				}
 
-                return time.Date(t.Year(), t.Month(), t.Day() + offset, 0, 0, 0, 0, time.Local).Format(format), nil
-            } else {
-                unit := m[2]
-                relativeTime[unit] += prefactor * numUnits
-            }
-        }
-    }
-    return t.
-    AddDate(relativeTime["YEAR"], relativeTime["MONTH"], relativeTime["TOMORROW"] -1 * relativeTime["YESTERDAY"] + relativeTime["DAY"] + relativeTime["WEEK"] * 7 + relativeTime["FORTNIGHT"] * 14).
-    Add(time.Duration(relativeTime["HOUR"]) * time.Hour + time.Duration(relativeTime["MINUTE"] + relativeTime["MIN"]) * time.Minute + time.Duration(relativeTime["SECOND"] + relativeTime["SEC"]) * time.Second).Local().Format(format), nil
+				return time.Date(t.Year(), t.Month(), t.Day()+offset, 0, 0, 0, 0, time.Local).Format(format), nil
+			} else {
+				unit := m[2]
+				relativeTime[unit] += prefactor * numUnits
+			}
+		}
+	}
+	return t.
+		AddDate(relativeTime["YEAR"], relativeTime["MONTH"], relativeTime["TOMORROW"]-1*relativeTime["YESTERDAY"]+relativeTime["DAY"]+relativeTime["WEEK"]*7+relativeTime["FORTNIGHT"]*14).
+		Add(time.Duration(relativeTime["HOUR"])*time.Hour + time.Duration(relativeTime["MINUTE"]+relativeTime["MIN"])*time.Minute + time.Duration(relativeTime["SECOND"]+relativeTime["SEC"])*time.Second).Local().Format(format), nil
 }
--- reldatelib_test.go
+++ reldatelib_test.go
@@ -5,221 +5,217 @@ import "testing"
 import "os/exec"
 import "strings"
 
-func runCmd(param string) (string) {
-    cmd := exec.Command("date", "-d", param)
-    var out bytes.Buffer
-    cmd.Stdout = &out
-    cmd.Run()
-    return strings.TrimSuffix(out.String(), "\n")
+func runCmd(param string) string {
+	cmd := exec.Command("date", "-d", param)
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	cmd.Run()
+	return strings.TrimSuffix(out.String(), "\n")
 }
 
 func cmpRes(t *testing.T, act string, exp string) {
-    if act != exp {
-        t.Errorf("output was incorrect, got: %q, want %q.", act, exp)
-    }
+	if act != exp {
+		t.Errorf("output was incorrect, got: %q, want %q.", act, exp)
+	}
 }
 
 func TestNow(t *testing.T) {
-    param := "now"
-    exp := runCmd(param)
-    act, _ := Parse(param)
-    cmpRes(t, act, exp)
+	param := "now"
+	exp := runCmd(param)
+	act, _ := Parse(param)
+	cmpRes(t, act, exp)
 }
 
 func TestToday(t *testing.T) {
-    param := "today"
-    exp := runCmd(param)
-    act, _ := Parse(param)
-    cmpRes(t, act, exp)
+	param := "today"
+	exp := runCmd(param)
+	act, _ := Parse(param)
+	cmpRes(t, act, exp)
 }
 
 func TestYesterday(t *testing.T) {
-    param := "yesterday"
-    exp := runCmd(param)
-    act, _ := Parse(param)
-    cmpRes(t, act, exp)
+	param := "yesterday"
+	exp := runCmd(param)
+	act, _ := Parse(param)
+	cmpRes(t, act, exp)
 }
 
 func TestTomorrow(t *testing.T) {
-    param := "tomorrow"
-    exp := runCmd(param)
-    act, _ := Parse(param)
-    cmpRes(t, act, exp)
+	param := "tomorrow"
+	exp := runCmd(param)
+	act, _ := Parse(param)
+	cmpRes(t, act, exp)
 }
 
 func TestWeek(t *testing.T) {
-    param := "week"
-    exp := runCmd(param)
-    act, _ := Parse(param)
-    cmpRes(t, act, exp)
+	param := "week"
+	exp := runCmd(param)
+	act, _ := Parse(param)
+	cmpRes(t, act, exp)
 }
 
 func TestCombo(t *testing.T) {
-    param := "20 DAYS 4 HOURS AGO 3 WEEKS"
-    exp := runCmd(param)
-    act, _ := Parse(param)
-    cmpRes(t, act, exp)
+	param := "20 DAYS 4 HOURS AGO 3 WEEKS"
+	exp := runCmd(param)
+	act, _ := Parse(param)
+	cmpRes(t, act, exp)
 }
 
 func TestNDaysAgo(t *testing.T) {
-    param := "2 days ago"
-    exp := runCmd(param)
-    act, _ := Parse(param)
-    cmpRes(t, act, exp)
+	param := "2 days ago"
+	exp := runCmd(param)
+	act, _ := Parse(param)
+	cmpRes(t, act, exp)
 }
 
 func TestNWeeksAgo(t *testing.T) {
-    param := "20 weeks ago"
-    exp := runCmd(param)
-    act, _ := Parse(param)
-    cmpRes(t, act, exp)
+	param := "20 weeks ago"
+	exp := runCmd(param)
+	act, _ := Parse(param)
+	cmpRes(t, act, exp)
 }
 
 func TestNegNWeeksAgo(t *testing.T) {
-    param := "-2 weeks ago"
-    exp := runCmd(param)
-    act, _ := Parse(param)
-    cmpRes(t, act, exp)
+	param := "-2 weeks ago"
+	exp := runCmd(param)
+	act, _ := Parse(param)
+	cmpRes(t, act, exp)
 }
 
 func TestNHoursAgo(t *testing.T) {
-    param := "2 hours ago"
-    exp := runCmd(param)
-    act, _ := Parse(param)
-    cmpRes(t, act, exp)
+	param := "2 hours ago"
+	exp := runCmd(param)
+	act, _ := Parse(param)
+	cmpRes(t, act, exp)
 }
 
 func TestNSecsAgo(t *testing.T) {
-    param := "2 secs ago"
-    exp := runCmd(param)
-    act, _ := Parse(param)
-    cmpRes(t, act, exp)
+	param := "2 secs ago"
+	exp := runCmd(param)
+	act, _ := Parse(param)
+	cmpRes(t, act, exp)
 }
 
 func TestNSecondsAgo(t *testing.T) {
-    param := "20 seconds ago"
-    exp := runCmd(param)
-    act, _ := Parse(param)
-    cmpRes(t, act, exp)
+	param := "20 seconds ago"
+	exp := runCmd(param)
+	act, _ := Parse(param)
+	cmpRes(t, act, exp)
 }
 
 func TestNMinAgo(t *testing.T) {
-    param := "15 min ago"
-    exp := runCmd(param)
-    act, _ := Parse(param)
-    cmpRes(t, act, exp)
+	param := "15 min ago"
+	exp := runCmd(param)
+	act, _ := Parse(param)
+	cmpRes(t, act, exp)
 }
 
 func TestNMinutesAgo(t *testing.T) {
-    param := "15 minutes ago"
-    exp := runCmd(param)
-    act, _ := Parse(param)
-    cmpRes(t, act, exp)
+	param := "15 minutes ago"
+	exp := runCmd(param)
+	act, _ := Parse(param)
+	cmpRes(t, act, exp)
 }
 
 func TestNFortnightsAgo(t *testing.T) {
-    param := "1 fortnight ago"
-    exp := runCmd(param)
-    act, _ := Parse(param)
-    cmpRes(t, act, exp)
+	param := "1 fortnight ago"
+	exp := runCmd(param)
+	act, _ := Parse(param)
+	cmpRes(t, act, exp)
 }
 
 func TestNextHour(t *testing.T) {
-    param := "next hour"
-    exp := runCmd(param)
-    act, _ := Parse(param)
-    cmpRes(t, act, exp)
+	param := "next hour"
+	exp := runCmd(param)
+	act, _ := Parse(param)
+	cmpRes(t, act, exp)
 }
 
 func TestNextHourAgo(t *testing.T) {
-    param := "next hour ago"
-    exp := runCmd(param)
-    act, _ := Parse(param)
-    cmpRes(t, act, exp)
+	param := "next hour ago"
+	exp := runCmd(param)
+	act, _ := Parse(param)
+	cmpRes(t, act, exp)
 }
 
 func TestNextFriday(t *testing.T) {
-    param := "next friday"
-    exp := runCmd(param)
-    act, _ := Parse(param)
-    cmpRes(t, act, exp)
+	param := "next friday"
+	exp := runCmd(param)
+	act, _ := Parse(param)
+	cmpRes(t, act, exp)
 }
 
 func TestNextThursday(t *testing.T) {
-    param := "next thursday"
-    exp := runCmd(param)
-    act, _ := Parse(param)
-    cmpRes(t, act, exp)
+	param := "next thursday"
+	exp := runCmd(param)
+	act, _ := Parse(param)
+	cmpRes(t, act, exp)
 }
 
 func TestNextSaturday(t *testing.T) {
-    param := "next saturday"
-    exp := runCmd(param)
-    act, _ := Parse(param)
-    cmpRes(t, act, exp)
+	param := "next saturday"
+	exp := runCmd(param)
+	act, _ := Parse(param)
+	cmpRes(t, act, exp)
 }
 
 func TestNextSunday(t *testing.T) {
-    param := "next sunday"
-    exp := runCmd(param)
-    act, _ := Parse(param)
-    cmpRes(t, act, exp)
+	param := "next sunday"
+	exp := runCmd(param)
+	act, _ := Parse(param)
+	cmpRes(t, act, exp)
 }
 
 func TestFirstFriday(t *testing.T) {
-    param := "first friday"
-    exp := runCmd(param)
-    act, _ := Parse(param)
-    cmpRes(t, act, exp)
+	param := "first friday"
+	exp := runCmd(param)
+	act, _ := Parse(param)
+	cmpRes(t, act, exp)
 }
 
 func TestThirdFriday(t *testing.T) {
-    param := "third friday"
-    exp := runCmd(param)
-    act, _ := Parse(param)
-    cmpRes(t, act, exp)
+	param := "third friday"
+	exp := runCmd(param)
+	act, _ := Parse(param)
+	cmpRes(t, act, exp)
 }
 
 func TestLastFriday(t *testing.T) {
-    param := "last friday"
-    exp := runCmd(param)
-    act, _ := Parse(param)
-    cmpRes(t, act, exp)
+	param := "last friday"
+	exp := runCmd(param)
+	act, _ := Parse(param)
+	cmpRes(t, act, exp)
 }
 
 func TestLastWednesday(t *testing.T) {
-    param := "last wednesday"
-    exp := runCmd(param)
-    act, _ := Parse(param)
-    cmpRes(t, act, exp)
+	param := "last wednesday"
+	exp := runCmd(param)
+	act, _ := Parse(param)
+	cmpRes(t, act, exp)
 }
 
 func TestLastThursday(t *testing.T) {
-    param := "last thursday"
-    exp := runCmd(param)
-    act, _ := Parse(param)
-    cmpRes(t, act, exp)
+	param := "last thursday"
+	exp := runCmd(param)
+	act, _ := Parse(param)
+	cmpRes(t, act, exp)
 }
 
 func TestLastSaturday(t *testing.T) {
-    param := "last saturday"
-    exp := runCmd(param)
-    act, _ := Parse(param)
-    cmpRes(t, act, exp)
+	param := "last saturday"
+	exp := runCmd(param)
+	act, _ := Parse(param)
+	cmpRes(t, act, exp)
 }
 
 func TestFail(t *testing.T) {
-    param := "potato"
-    cmd := exec.Command("date", "-d", param)
-    var out bytes.Buffer
-    cmd.Stdout = &out
-    cmd.Run()
+	param := "potato"
+	exp := runCmd(param)
+	act, err := Parse(param)
 
-    res, err := Parse(param)
-    if err == nil {
-        t.Errorf("expected error but got success")
-    }
-    if res != strings.TrimSuffix(out.String(), "\n") {
-        t.Errorf("output was incorrect, got: %q, want %q.", res, out.String())
-    }
+	if err == nil {
+		t.Errorf("expected error but got success")
+	}
+
+	cmpRes(t, act, exp)
 }
